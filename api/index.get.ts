5a3089c29eaca9685dbc93ccab7f7b1b82db1d85
Oyedeji Oyewole <57059138+oyedejioyewole@users.noreply.github.com>
2026-09-10 16:35:41 +0100

    fix(api): 🔧 stop returning `clientSecret` from `GET /api/`

--- api/index.get.ts
+++ api/index.get.ts
@@ -1,18 +1,14 @@
 import { defineHandler } from "nitro";
-import { HTTPError } from "nitro/h3";
 import { z } from "zod";
 
 import { db } from "#db/index.ts";
 import { allowedClients, selectSchema } from "#db/schema.ts";
 
 export default defineHandler(async () => {
-  const rows = await db.select().from(allowedClients).all();
-  const parsedRows = z.array(selectSchema).safeParse(rows);
-  if (!parsedRows.success)
-    throw new HTTPError("Failed to validate data", {
-      status: 500,
-      data: z.treeifyError(parsedRows.error),
-    });
+  const rows = db.select().from(allowedClients).all();
+  const parsedRows = z
+    .array(selectSchema.omit({ clientSecret: true }))
+    .parse(rows);
 
-  return parsedRows.data;
+  return parsedRows;
 });
